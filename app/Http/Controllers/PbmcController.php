150707323461b261925b08681dd6a3fdb175ebbf
Blessward Mutsotso <blessward.mutsotso@acrnhealth.com>
2026-02-07 15:36:27 +0200

    Artisan error

--- app/Http/Controllers/PbmcController.php
+++ app/Http/Controllers/PbmcController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Pbmc;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Log;
 use Throwable;
 
